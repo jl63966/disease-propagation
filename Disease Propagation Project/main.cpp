0b65eee0050ddc553cef5916fd5e09dd6cd06a9a
Jordan Landel <jordan.landel26@utexas.edu>
2020-10-27 16:39:27 -0500

    Initial Commit

---
+++ Disease Propagation Project/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Disease Propagation Project
+//
+//  Created by Jordan Landel on 10/27/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
